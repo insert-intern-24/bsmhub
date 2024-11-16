e76741c81309b5f1275384e328cc0ed50e62d2ef
gamzamandu <galbimandu1126@gmail.com>
2024-11-16 14:36:12 +0900

    user router 생성

---
+++ src/app/user/page.tsx
@@ -0,0 +1,13 @@
+// app/user/page.tsx
+import Background from '@components/shared/Background';
+import React from 'react';
+
+const UserPage = () => {
+	return (
+    <>
+      <Background/>
+    </>
+  );
+};
+
+export default UserPage;
