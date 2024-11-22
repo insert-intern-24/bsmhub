8bb647eca5059266014b051ce3aade38d2302ca8
gamzamandu <galbimandu1126@gmail.com>
2024-11-22 11:51:07 +0900

    refactor : user page -> layout, template, page로 분리

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,16 +1,13 @@
 // app/user/page.tsx
-import Background from '@components/shared/Background';
 import React from 'react';
-import Image from 'next/image';
+import Layout from '../layout';
+import UserTemplate from './template';
 
 const UserPage = () => {
-	return (
-    <>
-      <div>
-        <Image src="images/profile/default.svg" alt="default-profile" width={90} height={90}/>
-      </div>
-      <Background/>
-    </>
+  return (
+    <Layout>
+      <UserTemplate />
+    </Layout>
   );
 };
 
