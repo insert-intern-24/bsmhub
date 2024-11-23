c4a6b318220ff4d00f4def1cde17fc7a41c5d4eb
jaehwan02 <jhrick0129@gmail.com>
2024-11-24 01:32:06 +0900

    백그라운드 레이아웃 추가, ContestElement -> ContestItem으로 교체

--- src/app/components/shared/ContestItem.tsx
+++ src/app/components/shared/ContestItem.tsx
@@ -1,23 +1,23 @@
 import Image from 'next/image';
 
-interface ContestElementProps {
-	contestTitle: string;
-	contestStartDate: string;
-	contestEndDate: string;
+interface ContestItemProps {
+	title: string;
+	startDate: string;
+	endDate: string;
 }
 
-function ContestElement({ contestTitle, contestStartDate, contestEndDate }: ContestElementProps) {
+function ContestItem({ title, startDate, endDate }: ContestItemProps) {
 	return (
 		<div className="w-[21.25rem] h-fit border-solid border-[#E8E8EF] border-[1px] rounded-[5px]">
 			<Image src="/images/contest/Project.png" alt="contest1" width={340} height={200} />
 			<div className="w-full h-20 flex flex-col justify-center mx-3">
-				<div className="text-black text-[1rem] font-semibold leading-5">{contestTitle}</div>
+				<div className="text-titleColor text-[1rem] font-semibold leading-5">{title}</div>
 				<div className="text-detailColor text-[0.75rem] leading-4">
-					{contestStartDate} ~ {contestEndDate}
+					{startDate} ~ {endDate}
 				</div>
 			</div>
 		</div>
 	);
 }
 
-export default ContestElement;
+export default ContestItem;
--- src/app/contest/page.tsx
+++ src/app/contest/page.tsx
@@ -1,50 +1,47 @@
-import ContestElement from '@components/shared/ContestElement';
+import ContestItem from '@components/shared/ContestItem';
 
-const ContestElements = [
+const ContestItems = [
 	{
-		contestTitle: '상반기 AI캠프 해커톤',
-		contestStartDate: '2024.10.28(월)',
-		contestEndDate: '2024.11.29(화)',
+		title: '상반기 AI캠프 해커톤',
+		startDate: '2024.10.28(월)',
+		endDate: '2024.11.29(화)',
 	},
 	{
-		contestTitle: '하반기 AI캠프 해커톤',
-		contestStartDate: '2024.10.28(월)',
-		contestEndDate: '2024.11.29(화)',
+		title: '하반기 AI캠프 해커톤',
+		startDate: '2024.10.28(월)',
+		endDate: '2024.11.29(화)',
 	},
 	{
-		contestTitle: '상반기 AI캠프 해커톤',
-		contestStartDate: '2024.10.28(월)',
-		contestEndDate: '2024.11.29(화)',
+		title: '상반기 AI캠프 해커톤',
+		startDate: '2024.10.28(월)',
+		endDate: '2024.11.29(화)',
 	},
 	{
-		contestTitle: '하반기 AI캠프 해커톤',
-		contestStartDate: '2024.10.28(월)',
-		contestEndDate: '2024.11.29(화)',
+		title: '하반기 AI캠프 해커톤',
+		startDate: '2024.10.28(월)',
+		endDate: '2024.11.29(화)',
 	},
 	{
-		contestTitle: '상반기 AI캠프 해커톤',
-		contestStartDate: '2024.10.28(월)',
-		contestEndDate: '2024.11.29(화)',
+		title: '상반기 AI캠프 해커톤',
+		startDate: '2024.10.28(월)',
+		endDate: '2024.11.29(화)',
 	},
 	{
-		contestTitle: '하반기 AI캠프 해커톤',
-		contestStartDate: '2024.10.28(월)',
-		contestEndDate: '2024.11.29(화)',
+		title: '하반기 AI캠프 해커톤',
+		startDate: '2024.10.28(월)',
+		endDate: '2024.11.29(화)',
 	},
 ];
 
 function ContestPage() {
 	return (
-		<div className="flex flex-wrap gap-5">
-			{ContestElements.map((contest, key) => (
-				<ContestElement
-					key={key}
-					contestTitle={contest.contestTitle}
-					contestStartDate={contest.contestStartDate}
-					contestEndDate={contest.contestEndDate}
-				/>
-			))}
-		</div>
+		<>
+			<div className="flex flex-wrap gap-5">
+				{ContestItems.map((contest, key) => (
+					<ContestItem key={key} title={contest.title} startDate={contest.startDate} endDate={contest.endDate} />
+				))}
+			</div>
+		</>
 	);
 }
 
