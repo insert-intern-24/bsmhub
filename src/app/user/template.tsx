cca35e630de6d4867d9a9bf69341363624b5c32f
gamzamandu <galbimandu1126@gmail.com>
2024-11-25 15:20:28 +0900

    chore : ProjectItem 데이터 바인딩되게 수정, 이미지 예외 시 처리

--- src/app/user/template.tsx
+++ src/app/user/template.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import TabButton from '@components/TabButton';
 import DetailBoxes from '@components/detail/DetailBoxes';
+import ProjectItem from '@components/ProjectItem';
 
 const UserTemplate = () => {
 	return (
@@ -29,8 +30,17 @@ const UserTemplate = () => {
 						<TabButton tab="posts" />
 					</ul>
 				</nav>
-				<div className="w-full h-32 text-titleColor">마크다운 들어가는 섹션</div>
+				<div className="w-full max-h-32 text-titleColor mt-4">마크다운 입력하는 곳</div>
 				<DetailBoxes />
+			</section>
+			<section className="mt-2">
+				<h3 className="text-titleColor font-bold text-2xl">개인 프로젝트</h3>
+				<div className="flex gap-[0.7rem] flex-wrap">
+					<ProjectItem tag='개발완료' category='Desktop Utility' title='SANDDOET-App' description='산뜻 - AI 추천 RSS로 정보 습득이 쉬워지는 순간!'/>
+					<ProjectItem tag='개발완료' category='Desktop Utility' title='SANDDOET-App' description='산뜻 - AI 추천 RSS로 정보 습득이 쉬워지는 순간!'/>
+					<ProjectItem tag='개발완료' category='Desktop Utility' title='SANDDOET-App' description='산뜻 - AI 추천 RSS로 정보 습득이 쉬워지는 순간!'/>
+					<ProjectItem tag='개발완료' category='Desktop Utility' title='SANDDOET-App' description='산뜻 - AI 추천 RSS로 정보 습득이 쉬워지는 순간!'/>
+				</div>
 			</section>
 		</>
 	);
