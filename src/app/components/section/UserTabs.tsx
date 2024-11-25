5b6bdf8136c5036a513bab8e499b74f3e8bf9c9e
gamzamandu <galbimandu1126@gmail.com>
2024-11-25 22:14:33 +0900

    add : user-page의 tab별 이동 제작

---
+++ src/app/components/section/UserTabs.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import UserHome from '@components/section/UserHome';
+import UserProjects from '@components/section/UserProjects';
+import UserPosts from '@components/section/UserPosts';
+
+const tabComponents = {
+  home: <UserHome />,
+  project: <UserProjects />,
+  posts: <UserPosts />,
+};
+
+const UserTabs = () => {
+  const [tabName, setTabName] = useState('home');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const tab = urlParams.get('tab');
+    setTabName(tab && tabComponents[tab] ? tab : 'home');
+  }, [window.location.search]); 
+
+  return <div>{tabComponents[tabName]}</div>;
+};
+
+export default UserTabs;
--- src/app/user/template.tsx
+++ src/app/user/template.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import TabButton from '@components/TabButton';
-import DetailBoxes from '@components/detail/DetailBoxes';
-import ProjectItem from '@components/ProjectItem';
+import UserTabs from '@components/section/UserTabs';
 
 const UserTemplate = () => {
 	return (
@@ -30,17 +29,9 @@ const UserTemplate = () => {
 						<TabButton tab="posts" />
 					</ul>
 				</nav>
-				<div className="w-full max-h-32 text-titleColor mt-4">마크다운 입력하는 곳</div>
-				<DetailBoxes />
 			</section>
-			<section className="mt-2">
-				<h3 className="text-titleColor font-bold text-2xl">개인 프로젝트</h3>
-				<div className="flex gap-[0.7rem] flex-wrap">
-					<ProjectItem tag='개발완료' category='Desktop Utility' title='SANDDOET-App' description='산뜻 - AI 추천 RSS로 정보 습득이 쉬워지는 순간!'/>
-					<ProjectItem tag='개발완료' category='Desktop Utility' title='SANDDOET-App' description='산뜻 - AI 추천 RSS로 정보 습득이 쉬워지는 순간!'/>
-					<ProjectItem tag='개발완료' category='Desktop Utility' title='SANDDOET-App' description='산뜻 - AI 추천 RSS로 정보 습득이 쉬워지는 순간!'/>
-					<ProjectItem tag='개발완료' category='Desktop Utility' title='SANDDOET-App' description='산뜻 - AI 추천 RSS로 정보 습득이 쉬워지는 순간!'/>
-				</div>
+			<section>
+				<UserTabs/>
 			</section>
 		</>
 	);
