b10fb21cdfba722b92628f7bfc393413dba95eaf
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-01-08 13:46:51 +0900

    chore: article태그로 변환

--- src/app/components/community/feed/NotificationFeed.tsx
+++ src/app/components/community/feed/NotificationFeed.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import CardHeader from "@components/community/feed/header/CardHeader";
-import PostContent from "./content/PostContent";
-import PostReaction from "./reaction/PostReaction";
+import React from 'react';
+import CardHeader from '@components/community/feed/header/CardHeader';
+import PostContent from './content/PostContent';
+import PostReaction from './reaction/PostReaction';
 
 export default function NotificationFeed() {
   return (
     <>
-      <div className="flex w-full p-4 flex-col items-start gap-4 rounded-3xl bg-white">
+      <article className="flex w-full p-4 flex-col items-start gap-4 rounded-3xl bg-white">
         <CardHeader />
         <PostContent />
         <PostReaction />
-      </div>
+      </article>
     </>
   );
 }
