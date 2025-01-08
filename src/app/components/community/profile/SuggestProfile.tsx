a76b70418cab5852940910169ace9c101cda43d9
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-01-08 12:29:30 +0900

    feat: 추천 프로필 개발

---
+++ src/app/components/community/profile/SuggestProfile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Image from 'next/image';
+import timeline from '@public/images/icon/timeline.svg';
+import Profile from '@/app/components/community/profile/Profile';
+
+export default function SuggestProfile() {
+  const profiles = Array(6).fill(null);
+
+  return (
+    <>
+      <div className="flex px-1 flex-col justify-center items-start gap-[0.625rem] self-stretch">
+        <div className="flex items-center gap-[0.625rem]">
+          <Image
+            src={timeline}
+            alt="timeline"
+            width={(24 / 16) * 12}
+            height={(24 / 16) * 12}
+          />
+          <span className="text-black font-pretendard text-base font-normal leading-none">
+            Recommend Profiles
+          </span>
+        </div>
+        <div className="flex items-start gap-2">
+          {profiles.map((_, index) => (
+            <Profile key={index} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
