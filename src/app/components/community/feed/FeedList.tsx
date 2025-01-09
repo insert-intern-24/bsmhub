cea6c981c739356c83f703461c4a21c6362d6ebd
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-01-09 12:20:14 +0900

    feat: supabase 연동

--- src/app/components/community/feed/FeedList.tsx
+++ src/app/components/community/feed/FeedList.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import NotificationFeed from './NotificationFeed';
 import DefaultFeed from './DefaultFeed';
+import { createClient } from '@/utils/supabase/server';
 
-export default function FeedList() {
+export default async function FeedList() {
+  const supabase = await createClient();
+  let { data: community_post, error } = await supabase
+    .from('community_post')
+    .select('*');
+
+  if (error) {
+    console.error(error);
+    return <div>Error loading posts</div>;
+  }
+
   return (
     <>
-      <NotificationFeed
-        profileName={''}
-        projectStatus={''}
-        time={undefined}
-        profileImage={''}
-        projectName={''}
-        projectDescription={''}
-        contentImages={[]}
-      />
-      <DefaultFeed name={''} time={undefined} profileImage={''} text={''} />
+      {community_post?.map((post) => (
+        <DefaultFeed
+          key={post.id}
+          name={post.user_id}
+          time={post.created_at}
+          profileImage={''}
+          text={post.context}
+        />
+      ))}
     </>
   );
 }
