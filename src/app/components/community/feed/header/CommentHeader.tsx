0c6202bcbf21c934d5143e089baf17dba27504b5
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-01-09 11:40:48 +0900

    feat: 일반피드 props 추가

--- src/app/components/community/feed/header/CommentHeader.tsx
+++ src/app/components/community/feed/header/CommentHeader.tsx
@@ -1,23 +1,32 @@
-import React from "react";
-import Image from "next/image";
-import TeamProfile from "@public/images/icon/TeamProfile.svg";
+import React from 'react';
+import Image from 'next/image';
 
-export default function CommentHeader() {
+interface CommentHeaderProps {
+  name: string;
+  time: string;
+  profileImage: string;
+}
+
+export default function CommentHeader({
+  name,
+  time,
+  profileImage,
+}: CommentHeaderProps) {
   return (
     <>
       <div className="flex items-center gap-2 self-stretch">
         <Image
-          src={TeamProfile}
-          alt="TeamProfile"
+          src={profileImage}
+          alt="UserProfile"
           width={(44 / 16) * 12}
           height={(44 / 16) * 12}
         />
         <div className="flex items-center gap-1">
           <span className="text-black font-pretendard text-base font-bold leading-none">
-            이준호
+            {name}
           </span>
           <span className="text-descriptionColor font-pretendard text-xs font-normal leading-none">
-            2분전
+            {time}
           </span>
         </div>
         <span className="text-followBlue font-pretendard text-sm font-normal leading-none">
