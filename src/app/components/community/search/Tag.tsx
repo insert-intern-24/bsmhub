bf2039fb27963fa9d460363b636982b3a3b0ae96
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-01-09 11:41:03 +0900

    chore: 그대로

--- src/app/components/community/search/Tag.tsx
+++ src/app/components/community/search/Tag.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const TagName = [
-  { name: "전체" },
-  { name: "프로젝트" },
-  { name: "블로그" },
-  { name: "글" },
+  { name: '전체' },
+  { name: '프로젝트' },
+  { name: '블로그' },
+  { name: '글' },
 ];
 
 export default function Tag() {
-  const [selectedTag, setSelectedTag] = useState<string>("전체");
+  const [selectedTag, setSelectedTag] = useState<string>('전체');
 
   const handleClick = (tag: string) => {
     setSelectedTag(tag);
@@ -22,8 +22,8 @@ export default function Tag() {
           onClick={() => handleClick(tag.name)}
           className={`flex px-2 py-4 flex-col justify-center items-center gap-[0.625rem] cursor-pointer text-base font-normal leading-[0.5rem] font-pretendard ${
             selectedTag === tag.name
-              ? "bg-black text-white rounded-xl border border-black"
-              : "bg-white text-black rounded-xl border border-customGray"
+              ? 'bg-black text-white rounded-xl border border-black'
+              : 'bg-white text-black rounded-xl border border-customGray'
           }`}
         >
           {tag.name}
