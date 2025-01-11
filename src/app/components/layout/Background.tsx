6a2adc21c3749c068435b41186421507d81704ad
gamzamandu <galbimandu1126@gmail.com>
2025-01-11 18:07:05 +0900

    chore : layout 안의 div min-height로 변경

--- src/app/components/layout/Background.tsx
+++ src/app/components/layout/Background.tsx
@@ -5,7 +5,7 @@ const Background = () => {
     <>
     <div className='z-10'>
       <div className='h-28'></div> {/* div는 공간을 띄우기 위한 element입니다 */}
-      <div className="mx-auto bg-white w-full h-[calc(100dvh-7em)] text-black"></div>
+      <div className="mx-auto bg-white w-full min-h-[calc(100dvh-7em)] text-black"></div>
     </div>
     </>
   );
