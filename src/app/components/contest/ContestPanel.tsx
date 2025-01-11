866bd46f5dcf9b75dd86ac0adad82a8005591698
gamzamandu <galbimandu1126@gmail.com>
2025-01-12 00:43:00 +0900

    refactor : contestPanel.tsx의 불필요한 CSS 제거 및 대체, 시맨틱 태그로 변경, tailwind에서 제공하는 class 장려, 불필요한 div 및 속성 제거, 너무 긴 속성 tailwind.config.ts로 custom

--- src/app/components/contest/ContestPanel.tsx
+++ src/app/components/contest/ContestPanel.tsx
@@ -16,36 +16,32 @@ interface Contest {
 
 function SelectedContestPanel({ contest }: { contest: Contest }) {
   return (
-    <div className="w-[21.0625rem] flex flex-col shrink-0 [background:linear-gradient(180deg,rgba(47,66,205,0.07)_0%,rgba(255,255,255,0.07)_100%)]">
-      <div className="mx-10 flex flex-col mt-[12.5rem]">
-        <section className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-col gap-[0.4rem]">
-              <span className="text-[1.5rem] font-semibold">
-                {contest.title}
-              </span>
-              <span className="text-[1rem] font-normal text-detailColor">
-                {contest.startDate} ~ {contest.endDate}
-              </span>
-            </div>
-            <div className="w-full py-2 bg-titleColor rounded-[30px] flex justify-center items-center">
-              <div className="font-semibold text-white">투표하기</div>
-            </div>
+    <aside className="max-w-[21.0625rem] bg-contest-gradient px-10">
+      <div className="mt-[12.5rem]">
+        <header className="flex flex-col gap-4">
+          <div className="flex flex-col gap-[0.4rem]">
+            <span className="text-2xl font-bold">{contest.title} </span>
+            <span className="text-base text-detailColor">
+              {contest.startDate} ~ {contest.endDate}
+            </span>
           </div>
+          <button className="w-full py-2 bg-titleColor rounded-[30px] font-bold text-white">
+            투표하기
+          </button>
           <div className="flex flex-col gap-1 text-[1rem]">
             <ContestInformation name="대상" value="1학년" />
             <ContestInformation name="참여 인원" value="64" />
             <ContestInformation name="조회수" value="1089" />
           </div>
-        </section>
-        <hr className="w-full h-[1px] bg-strokeColor my-6"/>
-        <section className="flex flex-col gap-8">
+        </header>
+        <hr className="w-full h-[1px] bg-strokeColor my-6" />
+        <main className="flex flex-col gap-8">
           <ContestDetail description={contest.description} />
           <ContestDetail judges={contest.judges} />
           <ContestDetail awards={contest.awards} />
-        </section>
+        </main>
       </div>
-    </div>
+    </aside>
   );
 }
 
