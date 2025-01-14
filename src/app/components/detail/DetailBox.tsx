433ed21441b5e2cb0f2fe4f63103fd4fddffd0bc
gamzamandu <galbimandu1126@gmail.com>
2025-01-15 02:41:59 +0900

    chore : detail이 1개 이하일 경우 detailBox에서 갯수(length)를 표시하지 않도록 수정

--- src/app/components/detail/DetailBox.tsx
+++ src/app/components/detail/DetailBox.tsx
@@ -7,12 +7,12 @@ interface DetailBoxProps extends DetailType {
 	length?: number;
 }
 
-function DetailBox({ label, symbol, length, contents, type = "general" }: DetailBoxProps) {
+function DetailBox({ label, symbol, length = 0, contents, type = "general" }: DetailBoxProps) {
 	return (
 		<div className={`block max-w-[21.0625rem] w-full overflow-auto h-auto mb-[1.2rem] ${type === "general" && "min-h-[9rem] p-14px border border-strokeColor rounded-lg" }`}>
 			<div className="flex gap-2">
 				<p className="text-descriptionColor">{label}</p>
-				<span className="text-descriptionColor text-14px">{length}</span>
+				<span className="text-descriptionColor text-14px">{length > 1 ? length : ''}</span>
 			</div>
 			{contents.map((content, index) => (
 				<Detail 
