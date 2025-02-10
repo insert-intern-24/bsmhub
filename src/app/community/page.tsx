64a138339c6d917d4020d8e8ae4d6fb890307e03
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-02-10 22:45:36 +0900

    feat: StoryShare component 추가

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -4,12 +4,14 @@ import ProjectAd from '../components/community/additional/ProjectAd';
 import Search from '../components/community/search/Search';
 import SuggestProfile from '../components/community/profile/SuggestProfile';
 import FeedList from '../components/community/feed/FeedList';
+import StoryShare from '../components/community/sharestory/StoryShare';
 
 function MainCommunity() {
   return (
     <main className="flex gap-[0.625rem] py-[6.875rem]">
       <section className="flex w-full flex-col items-start gap-3 max-w-[66rem]">
         <Search />
+        <StoryShare />
         <FeedList />
         <SuggestProfile />
       </section>
