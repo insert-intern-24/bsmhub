e8030dde35e969482d8af2b0f7e23fb32e59158f
obtuse-triangle <me@obtuse.kr>
2025-02-13 04:25:54 +0900

    [I25-27] feat: 새 프로필 만들기 팝업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=favorite,home,search,settings" rel="stylesheet" />
+      </head>
       <body className="bg-[#F5F5F7]">
         <Header />
         <main className="mt-14 w-full min-h-dvh">
