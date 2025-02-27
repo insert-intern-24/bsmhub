36606fedb8c8bd5b841026a55c98836842547fd0
kwon5700 <kwon777071@gmail.com>
2025-02-27 22:01:16 +0900

    [I25-32] feat : 마크다운 데이터 바인딩

--- src/app/user/ProfileTemplate.tsx
+++ src/app/user/ProfileTemplate.tsx
@@ -7,8 +7,10 @@ import getProject from '@/services/project/getProject';
 
 import { profileType } from '../models/profile';
 import { categoryType, projectType } from '../models/project';
+import { Details } from '../models/collection'
 import getCategories from '@/services/project/getCategories';
 import getProfileMarkdown from '@/services/profile/getProfileMarkdown';
+import getDetails from '@/services/profile/getDetails';
 
 const UserTemplate = async ({ uuid }: { uuid: string }) => {
   const profile = await getProfileById(uuid) as profileType;
@@ -16,14 +18,16 @@ const UserTemplate = async ({ uuid }: { uuid: string }) => {
   let projects: projectType[] = [];
   let categories: categoryType[] = [];
   let markdown: string | null = null;
+  let details: Details | null = null;
 
   // profile id 있는지 확인 후 user 인지 유효성 검사
   if (!profile?.isTeam && profile?.profile_id) {
-    const fetchedProjects = await getProject(profile?.profile_id as string);
+    const fetchedProjects = await getProject(profile.profile_id as string);
     
     projects = fetchedProjects.map(item => item.projects);
     categories = (await getCategories(projects)).map(item => item.project_category);
     markdown = await getProfileMarkdown(profile.profile_id);
+    details = await getDetails(profile.profile_id);
   }
 
   // props 로 쉽게 넘기기 위해 userData 객체로 모았음
@@ -31,7 +35,8 @@ const UserTemplate = async ({ uuid }: { uuid: string }) => {
     profile,
     projects,
     categories,
-    markdown
+    markdown,
+    details
   };
 
   return (
---
+++ src/services/profile/getProfileMarkdown.ts
@@ -0,0 +1,21 @@
+'use server';
+import { createClient } from "@/utils/supabase/server";
+
+const getProfileMarkdown = async (profile_id: string) => {
+  const supabase = await createClient();
+  const {data, error} = await supabase
+    .schema('profile')
+    .from('profile_introduce')
+    .select('introduce')
+    .eq('profile_id', profile_id)
+
+  if (error) {
+    console.error('자기소개 조회 중 오류', error)
+    return "";
+  }
+
+
+  return data[0]?.introduce;
+}
+
+export default getProfileMarkdown
