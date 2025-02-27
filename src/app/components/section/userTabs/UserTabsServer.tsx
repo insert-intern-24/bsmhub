63e0c02fb8e0f0d34d74ae2baf5343a6d644865e
kwon5700 <kwon777071@gmail.com>
2025-02-27 22:02:38 +0900

    [I25-32] feat : project, profile 데이터 바인딩

--- src/app/components/section/userTabs/UserTabsServer.tsx
+++ src/app/components/section/userTabs/UserTabsServer.tsx
@@ -1,16 +1,21 @@
 // UserTabs.tsx (서버 컴포넌트)
 import tabsType from '../../tabs';
+import { userDataType } from '@/app/models/user';
 import { headers } from 'next/headers';
 
 import UserTabsTemplate from './UserTabsTemplate';
 
-const UserTabs = async () => {
+const UserTabs = async ({ userData }: { userData: userDataType}) => {
   const headersList = await headers();
   const tabMatch = headersList.get('x-url')?.match(/tab=(home|project|posts)/);
   const initialTab = (tabMatch ? tabMatch[1] : 'home') as tabsType;
   return (
     <>
-      <UserTabsTemplate tabName={initialTab} />
+      <UserTabsTemplate 
+        tabName={initialTab} 
+        profile={userData.profile}
+        projects={userData.projects}
+      />
     </>
   );
 };
