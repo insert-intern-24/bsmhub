27f364ea5a1437c8d766a0d4ad4461ed3419c878
galbimandu <galbimandu1126@gmail.com>
2025-02-28 18:39:58 +0900

    chore : 컴포넌트 폴더 정리 및 잘못된 이름 수정

--- src/app/components/profile/user/userTabs/UserTabsServer.tsx
+++ src/app/components/profile/user/userTabs/UserTabsServer.tsx
@@ -1,20 +1,17 @@
 // UserTabs.tsx (서버 컴포넌트)
-import tabsType from '../../tabs';
+import tabsType from '../../../tabs';
 import { userDataType } from '@/app/models/user';
 import { headers } from 'next/headers';
 
 import UserTabsTemplate from './UserTabsTemplate';
 
-const UserTabs = async ({ userData }: { userData: userDataType}) => {
+const UserTabs = async ({ userData }: { userData: userDataType }) => {
   const headersList = await headers();
   const tabMatch = headersList.get('x-url')?.match(/tab=(home|project|posts)/);
   const initialTab = (tabMatch ? tabMatch[1] : 'home') as tabsType;
   return (
     <>
-      <UserTabsTemplate 
-        tabName={initialTab} 
-        userData={userData}
-      />
+      <UserTabsTemplate tabName={initialTab} userData={userData} />
     </>
   );
 };
