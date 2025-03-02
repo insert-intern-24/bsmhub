498980b83899cd609c46f048728ac7dcb28e33d7
kwon5700 <kwon777071@gmail.com>
2025-03-03 08:00:54 +0900

    [I25-38] feat : 날짜 변환 함수 변경 및 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,21 @@
 export const formatDate = (data: string): string => {
+
   const date = new Date(data);
-    return new Intl.DateTimeFormat('ko-KR', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
+  
+  const formattedDate = new Intl.DateTimeFormat('ko-KR', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    weekday: 'short'
   }).format(date);
+
+  const [year, month, day, weekday] = formattedDate.split('. ');
+  return `${year}.${month}.${day} ${weekday}`
 };
+
+export const formatEndDate = (data: string | null): string => {
+  if(!data) return '';
+
+  data = formatDate(data);
+  return data ? ` ~ ${data}`:'';
+}
