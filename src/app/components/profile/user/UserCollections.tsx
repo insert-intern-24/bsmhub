054c687eefbec0f90ad6d3a65f898b7ab32fa565
kwon5700 <kwon777071@gmail.com>
2025-03-03 07:59:49 +0900

    [I25-38] chore : props 명 수정

--- src/app/components/profile/user/UserCollections.tsx
+++ src/app/components/profile/user/UserCollections.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { UserDataType } from '@/app/models/user';
 import CollectionItem from '../../collection/CollectionItem';
-import { formatDate } from '@/utils/date';
 
 const UserCollections = ({ userData }: { userData: UserDataType }) => {
 
@@ -10,12 +9,14 @@ const UserCollections = ({ userData }: { userData: UserDataType }) => {
       {userData?.collections.map((collection) => (
         <CollectionItem 
           key={collection.collection_id}
-          id={collection.collection_id}
-          title={collection.collection_name}
-          startDate={formatDate(collection.created_at)}
-          endDate={'2025년 3월 2일'}
-          thumbnail={collection.image_url}
-          onClick={() => alert("1")}
+          collection_id={collection.collection_id}
+          collection_name={collection.collection_name}
+          created_at={collection.created_at}
+          ended_at={collection.ended_at}
+          image_url={collection.image_url}
+          onClick={() => {
+            alert("1");
+          }}
           selected={false}
         />
       ))}
