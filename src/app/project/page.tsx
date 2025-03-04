f15d5eca8ae1bbd10c4c90e260bb02d188513e5b
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-03-04 22:56:11 +0900

    feat: 전체적인 props 개발

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Filter from '../components/project/filter/Filter';
 import Pagination from '../components/project/pagination/Pagination';
 import TotalList from '../components/project/totallist/TotalList';
+import { Projects } from '../models/projectSearch';
 
-export default function page() {
+export default function Page() {
+  const [projects, setProjects] = useState<Projects>([]);
   return (
     <>
       <div className="inline-flex pt-10 pr-16 pb-7 pl-[1.875rem] flex-col justify-end items-start gap-[1.125rem] bg-white">
         <span className="text-black text-[1.75rem] font-bold leading-none">
           프로젝트 모아보기
         </span>
-        <Filter />
-        <TotalList />
+        <Filter setProjects={setProjects} />
+        <TotalList projects={projects} />
         <Pagination />
       </div>
     </>
