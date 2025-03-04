c13fff1994546a384e59b4774454254689413619
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-03-04 22:54:07 +0900

    feat: 모든 props 바인딩과 supabase ProjectCard 필터링 문 추가

--- src/app/components/project/filter/Filter.tsx
+++ src/app/components/project/filter/Filter.tsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from './search/Search';
 import Tag from './tag/Tag';
+import { SearchQuery, Projects, Topics } from '@/app/models/projectSearch';
+import { createClient } from '@/utils/supabase/client';
 
-export default function Filter() {
+export default function Filter({
+  setProjects,
+}: {
+  setProjects: React.Dispatch<React.SetStateAction<Projects>>;
+}) {
+  const [searchQuery, setSearchQuery] = useState<SearchQuery>({});
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const supabase = createClient();
+      let query = supabase
+        .schema('project')
+        .from('projects')
+        .select('*, category_id!inner(*)');
+
+      if (searchQuery.inputQuery) {
+        query = query.textSearch('project_name', searchQuery.inputQuery);
+      }
+      if (searchQuery.selectedTags) {
+        const categoryTags = searchQuery.selectedTags
+          .filter((tag: Topics) => tag.id === 'category_id')
+          .map((tag: Topics) => tag.value);
+
+        const statusTags = searchQuery.selectedTags
+          .filter((tag: Topics) => tag.id === 'status')
+          .map((tag: Topics) => tag.value);
+
+        if (categoryTags.length > 0 && statusTags.length > 0) {
+          query = query
+            .in('category_id', categoryTags)
+            .in('status', statusTags);
+        } else if (categoryTags.length > 0) {
+          query = query.in('category_id', categoryTags);
+        } else if (statusTags.length > 0) {
+          query = query.in('status', statusTags);
+        }
+      }
+
+      const { data, error } = await query;
+
+      if (error) {
+        console.error('Error fetching search results:', error);
+        return;
+      } else {
+        setProjects(data);
+      }
+    };
+
+    fetchProjects();
+  }, [searchQuery]);
+
   return (
     <>
       <div className="w-[86.625rem] flex p-10 flex-col items-start gap-6 self-stretch rounded-xl border border-[#DADADA] bg-white">
-        <Search />
-        <Tag />
+        <Search setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
+        <Tag searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       </div>
     </>
   );
