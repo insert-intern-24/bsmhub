d73a23bc29d7f5fc5dad4cb3f70c1fb16ee49f4f
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-03-04 22:55:46 +0900

    feat: props로 필터링될 Card 전송

--- src/app/components/project/totallist/TotalList.tsx
+++ src/app/components/project/totallist/TotalList.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Total from './total/Total';
 import ProjectCard from './list/ProjectCard';
+import { Projects } from '@/app/models/projectSearch';
 
-export default function TotalList() {
+export default function TotalList({ projects }: { projects: Projects }) {
   return (
     <>
       <div className="flex flex-col items-start gap-6 self-stretch">
         <Total />
-        <ProjectCard />
+        <ProjectCard projects={projects} />
       </div>
     </>
   );
