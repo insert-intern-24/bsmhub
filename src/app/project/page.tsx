71bfea514e17f651326dd188700e2e184f08a54d
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-03-05 23:54:43 +0900

    feat: 나중 sort를 위한 useState추가

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,21 +1,20 @@
 'use client';
 import React, { useState } from 'react';
 import Filter from '../components/project/filter/Filter';
-import Pagination from '../components/project/pagination/Pagination';
 import TotalList from '../components/project/totallist/TotalList';
-import { Projects } from '../models/projectSearch';
+import { Projects, Newest } from '../models/projectSearch';
 
 export default function Page() {
   const [projects, setProjects] = useState<Projects>([]);
+  const [sort, setSort] = useState<Newest>('newest');
   return (
     <>
       <div className="inline-flex pt-10 pr-16 pb-7 pl-[1.875rem] flex-col justify-end items-start gap-[1.125rem] bg-white">
         <span className="text-black text-[1.75rem] font-bold leading-none">
           프로젝트 모아보기
         </span>
-        <Filter setProjects={setProjects} />
-        <TotalList projects={projects} />
-        <Pagination />
+        <Filter setProjects={setProjects} sort={sort} />
+        <TotalList projects={projects} setSort={setSort} />
       </div>
     </>
   );
