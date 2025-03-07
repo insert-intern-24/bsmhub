805b020b9978ed32b0b02b1beed59c4174181504
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-03-07 17:15:22 +0900

    feat: 중앙정렬

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -7,14 +7,17 @@ import { Projects, Sort } from '../models/projectSearch';
 export default function Page() {
   const [projects, setProjects] = useState<Projects>([]);
   const [sort, setSort] = useState<Sort>('Sort');
+
   return (
     <>
-      <div className="inline-flex pt-10 pr-16 pb-7 pl-[1.875rem] flex-col justify-end items-start gap-[1.125rem] bg-white">
-        <span className="text-black text-[1.75rem] font-bold leading-none">
-          프로젝트 모아보기
-        </span>
-        <Filter setProjects={setProjects} sort={sort} />
-        <TotalList projects={projects} setSort={setSort} />
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="inline-flex pt-10 pr-16 pb-7 pl-[1.875rem] flex-col gap-[1.125rem] bg-white ">
+          <span className="text-black text-[1.75rem] font-bold leading-none">
+            프로젝트 모아보기
+          </span>
+          <Filter setProjects={setProjects} sort={sort} />
+          <TotalList projects={projects} setSort={setSort} />
+        </div>
       </div>
     </>
   );
