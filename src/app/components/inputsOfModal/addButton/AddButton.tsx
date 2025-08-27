b7c81510f929489e995382e47787c7f86cf71c18
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-08-27 23:55:51 +0900

    style: background color add

--- src/app/components/inputsOfModal/addButton/AddButton.tsx
+++ src/app/components/inputsOfModal/addButton/AddButton.tsx
@@ -4,7 +4,7 @@ import { IconPlus } from '@tabler/icons-react';
 function AddButton() {
   return (
     <>
-      <div className="flex p-6 items-start gap-6 self-stretch rounded-2xl">
+      <div className="flex p-6 items-start gap-6 self-stretch rounded-2xl bg-light-gray-outline">
         <div className="flex items-center gap-1 self-stretch">
           <IconPlus size={14} stroke={2} color="gray" />
           <span className="text-gray-base text-sm font-normal leading-5 tracking-wider">
