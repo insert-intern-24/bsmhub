1b16029e709e07b96ec685063b3bb4be1757a10e
Jaehwan Lee <jhrick0129@gmail.com>
2025-09-06 17:55:50 +0900

    [I25-154] refactor : 유틸 사용

--- src/app/components/card/portfolio/LongPortfolioCard.tsx
+++ src/app/components/card/portfolio/LongPortfolioCard.tsx
@@ -7,13 +7,13 @@ import ProjectImages from './components/ProjectImages';
 
 const LongPortfolioCard = ({ profile, projects }: PortfolioCardProps) => {
   return (
-    <div className="flex justify-center items-center w-fit h-fit rounded bg-white">
-      <div className="flex flex-col justify-between items-center m-[1rem] gap-1">
+    <div className="flex-center w-fit h-fit rounded bg-white">
+      <div className="flex-col justify-between items-center m-[1rem] gap-1">
         <div className="flex gap-[0.625rem]">
           {/* Profile Image */}
           <ProfileImage src={profile.profile_image} name={profile.name} size="small" />
 
-          <div className="flex flex-col gap-2">
+          <div className="flex-col gap-2">
             {/* Profile Info */}
             <div className="relative w-full">
               <ProfileInfo profile={profile} layout="horizontal" />
