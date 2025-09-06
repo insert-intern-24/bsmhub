32e2109b78ec0e5b28581d18753c97113a928176
Jaehwan Lee <jhrick0129@gmail.com>
2025-09-07 01:52:42 +0900

    [I25-154] fix : tabler아이콘 적용

--- src/app/components/card/project/ProjectAddCard.tsx
+++ src/app/components/card/project/ProjectAddCard.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import Image from 'next/image';
+import { IconPlus } from '@tabler/icons-react';
 
 const ProjectAddCard = () => {
   return (
-    <div className="w-fit flex flex-col gap-[0.375rem]">
-      <div className="relative w-[21rem] h-[11.8125rem] rounded-[0.25rem] bg-light-gray-outline flex items-center justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Image
-            src="/card/add.png"
-            alt="프로젝트 추가"
-            width={(76 * 12) / 16}
-            height={(76 * 12) / 16}
-          />
-        </div>
+    <div className="w-[21rem] h-[13.4375rem] rounded-[0.25rem] bg-light-gray-outline flex items-center justify-center">
+      <div className="flex-col flex-center gap-2">
+        <IconPlus size={(76 * 12) / 16} color="#EAEAEC" />
       </div>
-      {/* MetaInfo 영역과 동일한 높이 확보 */}
-      <div className="h-[1.25rem]"></div>
     </div>
   );
 };
