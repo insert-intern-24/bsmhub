d4e242376b0f9e4efc6ab1df437d8af2ceb8ed25
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-09-08 15:51:43 +0900

    feat: date type의 아이콘 커스텀 아이콘으로 수정

--- src/app/components/inputsOfModal/inputs/Inputs.tsx
+++ src/app/components/inputsOfModal/inputs/Inputs.tsx
@@ -1,9 +1,9 @@
 'use client';
 import React from 'react';
-import { IconPencil } from '@tabler/icons-react';
-import { IconCheck } from '@tabler/icons-react';
-import { IconSearch } from '@tabler/icons-react';
+import { IconPencil, IconCheck, IconSearch } from '@tabler/icons-react';
 import { BaseInputProps } from '../types/inputTypes';
+import Image from 'next/image';
+import Calendar from '@public/icon/calendar.svg';
 
 function Inputs({
   type = 'text',
@@ -28,6 +28,16 @@ function Inputs({
         return <IconCheck size={20} className="text-gray-500" />;
       case 'search':
         return <IconSearch size={20} className="text-gray-500" />;
+      case 'date':
+        return (
+          <Image
+            src={Calendar}
+            width={20}
+            height={20}
+            alt="Calendar"
+            className="text-gray-500"
+          />
+        );
       default:
         return null;
     }
@@ -45,23 +55,27 @@ function Inputs({
         required={required}
         readOnly={type === 'lock'}
         className={`w-full h-[3.3125rem] py-1 ${
-          ['lock', 'edit', 'search'].includes(type) ? 'pr-10' : 'px-2.5'
+          ['lock', 'edit', 'search', 'date'].includes(type) ? 'pr-10' : 'px-2.5'
         } pl-2.5 rounded-md text-gray-base text-base font-normal leading-6 tracking-[0.0057rem] ${
           type !== 'lock' ? 'bg-light-gray-outline' : ''
         } ${type === 'date' ? 'date-input' : ''}`}
       />
       {renderIcon() && (
-        <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+        <div className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none">
           {renderIcon()}
         </div>
       )}
 
       <style jsx>{`
         .date-input::-webkit-calendar-picker-indicator {
+          opacity: 0;
+          width: 28px;
+          height: 28px;
           position: absolute;
-          right: 10px;
+          right: 4px;
           top: 50%;
           transform: translateY(-50%);
+          cursor: pointer;
         }
         .date-input::-webkit-datetime-edit {
           padding-right: 30px;
