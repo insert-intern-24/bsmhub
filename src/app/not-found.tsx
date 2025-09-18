69b5fb5b3d3d6f0e1235ec3b9e065da770adb711
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-09-18 14:42:28 +0900

    style: comment 반영 gap-4를 div에 전체 적용 후 mb는 body에만 4로 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,9 +4,9 @@ import { Title, Body } from '@/app/components/system/text';
 
 export default function NotFound() {
   return (
-    <div className="flex-center-all h-screen">
-      <Title className="mb-4">404 - 페이지를 찾을 수 없습니다</Title>
-      <Body className="mb-8">요청하신 페이지가 존재하지 않습니다</Body>
+    <div className="flex-center-all h-screen gap-4">
+      <Title>404 - 페이지를 찾을 수 없습니다</Title>
+      <Body className="mb-4">요청하신 페이지가 존재하지 않습니다</Body>
       <Link
         href="/"
         className="px-6 py-3 bg-blue-primary text-white rounded-full"
