5d707d5b5168d36701a13059ceba3d72f610993b
kwonminjae5700 <kwon777071@gmail.com>
2025-10-08 03:35:16 +0900

    [I25-170] style : 크기 조정

--- src/app/components/card/project/ProjectCard.tsx
+++ src/app/components/card/project/ProjectCard.tsx
@@ -14,8 +14,8 @@ interface CardProps {
 
 const Card = ({ title, projectImage, authors }: CardProps) => {
   return (
-  <div className="w-fit flex-col gap-[0.375rem]">
-      <figure className="relative w-[21rem] h-[11.8125rem] rounded-[0.25rem] overflow-hidden">
+    <div className="w-fit flex-col gap-[0.375rem]">
+      <figure className="relative w-[24rem] h-[13rem] rounded-[0.25rem] overflow-hidden">
         <Image
           src={projectImage}
           alt={`${title} 프로젝트 이미지`}
