e92802d2c43af84e722f0b39d48f6967d1a17c31
gamzamandu <galbimandu1126@gmail.com>
2025-10-08 15:40:21 +0900

    [I25-152] feat: Checkbox 컴포넌트 구현
    - 체크박스 인풋 컴포넌트 추가
    - 모달 인풋 시스템에 체크박스 타입 지원

--- src/app/components/modal/inputsOfModal/Checkbox.tsx
+++ src/app/components/modal/inputsOfModal/Checkbox.tsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react'
-import { IconSquareCheckFilled, IconSquareCheck } from '@tabler/icons-react'
+import React, { useState } from 'react';
+import { IconSquareCheckFilled, IconSquareCheck } from '@tabler/icons-react';
 
 const Checkbox = () => {
-    const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useState(false);
 
-    return (
-        <label className='flex flex-row items-center gap-2 cursor-pointer select-none' onClick={() => setChecked(!checked)}>
-            {
-                checked ? (
-                    <IconSquareCheckFilled className='w-8' />
-                ) : (
-                    <IconSquareCheck className='w-8 bg-transparent' />
-                )
-            } {'체크박스'}
-        </label>
-    )
-}
+  return (
+    <label
+      className="flex flex-row items-center gap-2 cursor-pointer select-none"
+      onClick={() => setChecked(!checked)}
+    >
+      {checked ? (
+        <IconSquareCheckFilled className="w-8" />
+      ) : (
+        <IconSquareCheck className="w-8" />
+      )}{' '}
+      {'체크박스'}
+    </label>
+  );
+};
 
-export default Checkbox
+export default Checkbox;
