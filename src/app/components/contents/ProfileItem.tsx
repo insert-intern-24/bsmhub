67bc1bd5c307d9c563dbc52d42eb68ba75cb935b
kwonminjae5700 <kwon777071@gmail.com>
2025-10-09 04:00:11 +0900

    [I25-170] fix : 링크 alt 대체와 prize 속성 추가

--- src/app/components/contents/ProfileItem.tsx
+++ src/app/components/contents/ProfileItem.tsx
@@ -1,30 +1,28 @@
-import React from "react"
-import Link from "next/link"
-import { IconLink } from "@tabler/icons-react"
-import { ItemProps } from "@/types/portfolio"
+import React from 'react';
+import Link from 'next/link';
+import { IconLink } from '@tabler/icons-react';
+import { ItemProps } from '@/app/portfolio/types/portfolio';
 
-const ProfileItem = ({ mode, value, url }: ItemProps) => {
+const ProfileItem = ({ mode, value, url, prize }: ItemProps) => {
   const Content = (
-    <div className='max-w-fit text-gray-base flex-center gap-0.5 text-sm font-normal'>
-      {mode === 'link' && (
-        <IconLink 
-          width={10}
-          height={10}
-        />
-      )}
-      {value}
+    <div className="max-w-fit text-gray-base flex-center gap-0.5 text-sm font-normal">
+      {mode === 'link' && <IconLink width={10} height={10} />}
+      {value ?? url}
+      {` ${prize ?? ''}`}
     </div>
-  )
+  );
 
   return url ? (
     <Link
       href={url}
-      target='_blank'
-      className='outline-none border-none inline-flex max-w-fit'
-    >{Content}</Link>
+      target="_blank"
+      className="outline-none border-none inline-flex max-w-fit"
+    >
+      {Content}
+    </Link>
   ) : (
     Content
-  )
-}
+  );
+};
 
 export default ProfileItem;
