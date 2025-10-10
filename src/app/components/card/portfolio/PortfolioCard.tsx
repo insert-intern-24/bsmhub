b7af61eefeaceca5ffc36298ac777e8f5f35b6ef
Obtuse triangle <me@obtuse.kr>
2025-10-11 03:28:10 +0900

    [I25-177] chore: PortfolioCard UI개선

--- src/app/components/card/portfolio/PortfolioCard.tsx
+++ src/app/components/card/portfolio/PortfolioCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 import { PortfolioCardProps } from './types';
 import ProfileInfo from './components/ProfileInfo';
 import StatusBadge from './components/StatusBadge';
@@ -8,58 +7,54 @@ import ProfileImage from './components/ProfileImage';
 
 const PortfolioCard = ({ profile, projects }: PortfolioCardProps) => {
   return (
-    <div className="@container w-full h-fit">
+    <div className="@container w-full h-fit rounded border-light-gray-outline bg-white p-4">
+      {/* 컨테이너가 412px 미만일 때: 세로 레이아웃 (기존 PortfolioCard) */}
       <div
-        className="flex-center w-full h-fit rounded border-light-gray-outline"
+        className="@[412px]:hidden w-full h-fit flex-col flex-center"
         style={{
           background:
             'linear-gradient(180deg, #f9f9fb 26%, #fff 67.5%, #fff 100%)',
         }}
       >
-        {/* 컨테이너가 300px 미만일 때: 세로 레이아웃 (기존 PortfolioCard) */}
-        <div className="@[300px]:hidden w-full h-fit flex-col flex-center m-[0.865rem] mb-[1rem]">
-          {/* Project Images Header */}
-          <ProjectImages projects={projects} variant="default" />
+        {/* Project Images Header */}
+        <ProjectImages projects={projects} variant="default" />
 
-          {/* Profile Section */}
-          <div className="relative ml-2 w-full">
-            <Image
-              src={profile.profile_image}
-              alt={`${profile.name} 프로필`}
-              width={(60 * 12) / 16}
-              height={(60 * 12) / 16}
-              className="rounded-full object-cover absolute top-[-4.25rem]"
-            />
-            <ProfileInfo profile={profile} layout="horizontal" />
-            <StatusBadge
-              status={profile.status}
-              className="absolute top-[-0.375rem] right-0"
-            />
-          </div>
-        </div>
-
-        {/* 컨테이너가 300px 이상일 때: 가로 레이아웃 (기존 LongPortfolioCard) */}
-        <div className="hidden @[300px]:flex @[300px]:w-full @[300px]:h-fit @[300px]:m-[1rem] @[300px]:gap-1">
-          {/* Profile Image */}
+        {/* Profile Section */}
+        <div className="relative ml-2 w-full">
           <ProfileImage
             src={profile.profile_image}
             name={profile.name}
             size="small"
           />
+          <ProfileInfo profile={profile} layout="horizontal" />
+          <StatusBadge
+            status={profile.status}
+            className="absolute top-0 right-0"
+          />
+        </div>
+      </div>
 
-          <div className="flex-col gap-2 w-full">
-            {/* Profile Info */}
-            <div className="relative w-full">
-              <ProfileInfo profile={profile} layout="horizontal" />
-              <StatusBadge
-                status={profile.status}
-                className="absolute top-0 right-0"
-              />
-            </div>
+      {/* 컨테이너가 412px 이상일 때: 가로 레이아웃 (기존 LongPortfolioCard) */}
+      <div className="hidden @[412px]:flex w-full h-fit gap-2.5">
+        {/* Profile Image */}
+        <ProfileImage
+          src={profile.profile_image}
+          name={profile.name}
+          size="small"
+        />
 
-            {/* Project Images */}
-            <ProjectImages projects={projects} variant="long" />
+        <div className="flex-col gap-2 flex-1">
+          {/* Profile Info */}
+          <div className="relative w-full">
+            <ProfileInfo profile={profile} layout="horizontal" />
+            <StatusBadge
+              status={profile.status}
+              className="absolute top-0 right-0"
+            />
           </div>
+
+          {/* Project Images */}
+          <ProjectImages projects={projects} variant="long" />
         </div>
       </div>
     </div>
