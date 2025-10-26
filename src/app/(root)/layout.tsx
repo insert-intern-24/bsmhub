d92da4162548bdeb78fbbf6667e131ed65d0ada5
gamzamandu <galbimandu1126@gmail.com>
2025-10-26 14:41:12 +0900

    [I25-212] feat: 헤더 고정 및 레이아웃 조정
    - 헤더를 화면 상단에 고정 (fixed position)
    - 로고 및 타이틀 전체를 클릭 가능한 홈 링크로 변경
    - 헤더 고정으로 인한 레이아웃 조정 (body에 pt-14 추가)
    - 각 레이아웃의 패딩 값 조정하여 콘텐츠가 헤더에 가려지지 않도록 수정

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -15,7 +15,7 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="w-full min-h-dvh pt-[4.5rem] bg-white">
+    <main className="w-full min-h-dvh bg-white">
       <div className="max-w-[109rem] px-[2.75rem] mobile:p-3 min-h-screen mobile:pb-12 pb-[4rem] mx-auto">
         {children}
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
       </head>
-      <body className="bg-[#F5F5F7]">
+      <body className="bg-[#F5F5F7] pt-14">
         <ModalProvider>
           <Header />
           {children}
