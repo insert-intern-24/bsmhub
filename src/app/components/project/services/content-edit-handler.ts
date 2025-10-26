2f522f1621523a430cceaccef6de6b51059ac5d8
Obtuse triangle <me@obtuse.kr>
2025-10-27 01:28:53 +0900

    [I25-206] fix: 비동기 함수로 변경하여 프로젝트 내용 업데이트 로직 개선

--- src/app/components/project/services/content-edit-handler.ts
+++ src/app/components/project/services/content-edit-handler.ts
@@ -1,19 +1,17 @@
 import { createClient } from '@/utils/supabase/client';
 
-export default function projectContentEditHandler(
+export default async function projectContentEditHandler(
   project_id: number,
   content: string,
 ) {
-  const supabase = createClient();
-  supabase
+  const supabase = await createClient();
+  const { error } = await supabase
     .from('project_html_description')
-    .update({ html_content: content })
-    .eq('project_id', project_id)
-    .then((response) => {
-      if (response.error) {
-        console.error('프로젝트 내용 업데이트 실패:', response.error);
-      } else {
-        console.log('프로젝트 내용이 성공적으로 업데이트되었습니다.');
-      }
-    });
+    .update({ html_content: content } as never)
+    .eq('project_id', project_id);
+  if (error) {
+    console.error('프로젝트 내용 업데이트 실패:', error);
+  } else {
+    console.log('프로젝트 내용이 성공적으로 업데이트되었습니다.');
+  }
 }
