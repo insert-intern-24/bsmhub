812a432f8ef8ef557ce78a143b7f0939cd215613
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-10-26 15:59:34 +0900

    refactor: providers.tsx 파일 추가 후 use client 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,60 +1,41 @@
-'use client';
-
 import Footer from './components/layout/Footer';
 import './globals.css';
 import './responsive.css';
 import '@components/modal/inputs/common/common.css';
 import '@components/modal/modal.css';
 import Header from '@components/layout/Header';
-import { ModalProvider, Modal } from '@components/modal';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
 import Navigator from '@components/layout/Navigator';
+import Providers from './providers';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'BSMHub',
+  description: '부산소프트웨어마이스터고 프로젝트의 장',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 5 * 60 * 1000,
-            gcTime: 10 * 60 * 1000,
-            retry: 3,
-            refetchOnWindowFocus: false,
-          },
-        },
-      }),
-  );
-
   return (
     <html lang="ko">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>BSMHub</title>
-        <meta
-          name="description"
-          content="부산소프트웨어마이스터고 프로젝트의 장"
-        />
-        <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
       </head>
       <body className="bg-[#F5F5F7] pt-14">
-        <QueryClientProvider client={queryClient}>
-          <ModalProvider>
-            <Header />
-            {children}
-            <Navigator />
-            <Footer />
-            <Modal />
-          </ModalProvider>
-        </QueryClientProvider>
+        <Providers>
+          <Header />
+          {children}
+          <Navigator />
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
