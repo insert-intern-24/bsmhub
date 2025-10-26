34061da130cea90441d3c84843d1b3b4530c65da
Obtuse triangle <me@obtuse.kr>
2025-10-27 03:22:36 +0900

    [I25-214] fix: 필터 사이드바의 패딩 및 최소 너비 조정, 반응형 기준 조정

--- src/app/(box-layout)/portfolio/SearchTab.tsx
+++ src/app/(box-layout)/portfolio/SearchTab.tsx
@@ -96,9 +96,9 @@ export default function SearchTab({
     .filter(filterByJobs)
     .filter(filterByEmploymentStatus);
   return (
-    <div className="py-9 flex flex-col md:flex-row gap-4 min-h-dvh w-full">
+    <div className="pt-9 flex mobile:flex-col flex-row gap-4 min-h-dvh w-full">
       {/* 검색 및 필터 사이드바 */}
-      <aside className="flex-col gap-3 min-w-64">
+      <aside className="flex-col gap-3 min-w-80">
         <Inputs
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search"
