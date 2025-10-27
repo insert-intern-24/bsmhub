0d8a88b28c0e17139d66bb63c85bf967764c4c47
Obtuse triangle <me@obtuse.kr>
2025-10-27 23:02:27 +0900

    [I25-217] feat: 구글 OAuth 로그인 로직 팝업으로 이동

--- src/app/components/auth/Account.tsx
+++ src/app/components/auth/Account.tsx
@@ -4,47 +4,54 @@ import { createClient } from '@/utils/supabase/client';
 import { useEffect, useState } from 'react';
 import { useModal } from '@/app/components/modal';
 import InputOfModal from '@/app/components/modal/inputs/InputOfModal';
-import { useProfileExistence } from '@/utils/hook/useProfile';
 import { Dropdown, DropdownItem } from '@/app/components/dropdown/DropDown';
 import { profileConfig } from '@/services/config/profileConfig';
 import Image from 'next/image';
 import { User } from '@supabase/supabase-js';
+import { checkProfileExistence } from '@/services/client/profile/profileApi';
 
 const Account = () => {
   const supabase = createClient();
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userProfile, setUserProfile] = useState<User | null>(null);
   const { openModal, closeModal } = useModal();
-
-  const { data: profileExists } = useProfileExistence();
 
   useEffect(() => {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange(async (event, session) => {
       setUserProfile(session?.user || null);
+      // 첫 로그인 시 프로필 존재 여부 확인
+      if (event === 'SIGNED_IN') {
+        checkProfileExistence(session!.user.id).then((exists) => {
+          if (exists) return;
+          openModal(
+            <InputOfModal
+              title="프로필 설정"
+              config={profileConfig}
+              onSubmit={() => closeModal()}
+            />,
+          );
+        });
+      }
     });
     return () => subscription.unsubscribe();
   }, []);
 
-  useEffect(() => {
-    if (userProfile && profileExists === false) {
-      openModal(
-        <InputOfModal
-          title="프로필 설정"
-          config={profileConfig}
-          onSubmit={() => closeModal()}
-        />,
-      );
-    }
-  }, [userProfile, profileExists]);
-
   const handleGoogleLogin = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'google',
-      options: {
-        redirectTo: `${window.location.origin}/`,
-      },
+    const popup = window.open(
+      `${window.location.origin}/auth/login`,
+      '_blank',
+      'popup,scrollbars=yes,resizable=yes,width=500,height=800',
+    );
+
+    popup?.focus();
+
+    window.addEventListener('message', (event) => {
+      if (event.origin !== window.location.origin) return;
+
+      if (event.data === 'success') {
+        popup?.close();
+      }
     });
   };
 
