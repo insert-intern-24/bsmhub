5cecc9e8f932f78a9725536a05f6ac3f9689b1b7
Jaehwan Lee <jhrick0129@gmail.com>
2025-10-28 19:21:19 +0900

    [I25-209] feat : 중간 배너 모바일 화면에 표시

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -11,8 +11,8 @@ export default async function Home() {
       <div className="flex-center">
         <div className="h-[14.75rem] w-full justify-between flex mobile:justify-center">
           <div className="h-full w-[53.5rem] mobile:hidden bg-gray-50 rounded-[0.25rem]"></div>
-          <div className="h-full w-[26rem] mobile:hidden bg-gray-50 rounded-[0.25rem]"></div>
           <div className="h-full w-[26rem] mobile:w-full mobile:h-[15rem] bg-gray-50 rounded-[0.25rem]"></div>
+          <div className="h-full w-[26rem] mobile:hidden bg-gray-50 rounded-[0.25rem]"></div>
         </div>
       </div>
       <ProjectList projects={projects} />
