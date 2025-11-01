3b5542f46310c68fea52c2c87d3cee3a05b7403d
Obtuse triangle <me@obtuse.kr>
2025-11-01 14:49:23 +0900

    fix: Dropdown에서 프로필 만들기 항목 제거

--- src/app/components/auth/Account.tsx
+++ src/app/components/auth/Account.tsx
@@ -228,7 +228,6 @@ const Account = () => {
       }
       align="right"
     >
-      <DropdownItem onSelect={handleMakeProfile}>프로필 만들기</DropdownItem>
       {profileLink ? (
         <Link href={profileLink}>
           <DropdownItem>내 프로필</DropdownItem>
