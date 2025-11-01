bc840aa63f1c22c917a341fa9cf9a291422659c6
Obtuse triangle <me@obtuse.kr>
2025-11-01 15:25:05 +0900

    [I25-235] feat: 사용자 계정 정보에 따라 HeroCard 또는 LoginBox 표시

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,9 +3,13 @@ import { getProjects } from '@/services/server/project/getProjects';
 import OneTapComponent from '@/app/components/auth/GoogleOneTab';
 import LoginBox from '@/app/components/auth/LoginBox';
 import AutoSlidingBusinessCard from '../components/card/root/AutoSlidingBusinessCard';
+import HeroCard from '../components/card/HeroCard';
+import getMyAccount from '@/services/server/auth/getMyAccount';
 
 export default async function Home() {
   const projects = await getProjects();
+
+  const user = await getMyAccount();
 
   return (
     <div className="flex-col gap-4 w-full">
@@ -14,7 +18,7 @@ export default async function Home() {
         <div className="h-[14.75rem] w-full justify-between flex mobile:justify-center">
           <div className="h-full w-[53.5rem] mobile:hidden bg-gray-50 rounded-[0.25rem]"></div>
           <AutoSlidingBusinessCard />
-          <LoginBox />
+          {user ? <HeroCard /> : <LoginBox />}
         </div>
       </div>
       <ProjectList projects={projects} />
