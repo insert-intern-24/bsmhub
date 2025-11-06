5ed8d43e4c6e1494a232ab46829d5f4a5ecf8ec7
gamzamandu <galbimandu1126@gmail.com>
2025-11-07 02:47:01 +0900

    [I25-238] feat: 프로필 이미지 컴포넌트에서 URL 변환 함수 일관성 있게 적용

--- src/app/components/card/portfolio/ProfileIcon.tsx
+++ src/app/components/card/portfolio/ProfileIcon.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image';
 import { convertFromDatabaseImageURL } from '@/utils/supabase/imageHostConverter';
 
 const ProfileIcon = ({ image }: { image: string }) => {
+  const imageUrl = convertFromDatabaseImageURL(image);
   return (
     <Image
       width={(120 / 16) * 14}
       height={(120 / 16) * 14}
       alt="프로필 사진"
-      src={convertFromDatabaseImageURL(image)}
+      src={imageUrl}
       className="rounded-sm absolute -top-20"
     />
   );
