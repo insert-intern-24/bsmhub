6b30897fd2d77bc34b1cb2321365720e964c50c0
gamzamandu <galbimandu1126@gmail.com>
2025-11-13 12:26:55 +0900

    [I25-256] feat : 타입 파일 및 프로젝트 사이드바 컴포넌트 추가

---
+++ src/app/(box-layout)/team/types.ts
@@ -0,0 +1,20 @@
+import { PersonalProjectType } from "@/services/project/getPersonalProjects.server";
+import { Tables } from "@/services/supabase/database.types";
+import { MergeDeep } from "type-fest";
+
+export type TeamData = MergeDeep<
+  Omit<Tables<'profile'>, 'email' | 'is_team' | 'link'>,
+  {
+    profile_link: Pick<Tables<'profile_link'>, 'link' | 'alt'>[];
+    team_member: {
+      profile: Pick<Tables<'profile'>, 'profile_id' | 'profile_image'>;
+    }[];
+  }
+>;
+
+export type TeamProjectType = PersonalProjectType & {
+  profile: Pick<Tables<'profile'>, 'profile_name' | 'is_team'>;
+  project_contributors: {
+    profile: Pick<Tables<'profile'>, 'profile_id' | 'profile_image' | 'profile_name'>;
+  }[];
+}
