a79f89592c91638391272da9d1366d809fe58f57
Obtuse triangle <me@obtuse.kr>
2025-11-13 13:54:06 +0900

    revert: clint.ts

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -5,21 +5,5 @@ export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return document.cookie
-            .split('; ')
-            .find(row => row.startsWith(`${name}=`))
-            ?.split('=')[1];
-        },
-        set(name: string, value: string, options: any) {
-          document.cookie = `${name}=${value}; path=/; ${options.maxAge ? `max-age=${options.maxAge}` : ''}; SameSite=Lax`;
-        },
-        remove(name: string, options: any) {
-          document.cookie = `${name}=; path=/; max-age=0`;
-        },
-      },
-    }
   );
 }
