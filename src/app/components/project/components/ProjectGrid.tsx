113ef6cfa2749b8c47480e69522e612540c0a5e3
kwonminjae5700 <kwon777071@gmail.com>
2025-11-17 15:02:23 +0900

    [I25-242] refactor: 프로젝트 grid 컴포넌트 생성 후 통일

---
+++ src/app/components/project/components/ProjectGrid.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { CardProps } from '@/app/components/card/project/ProjectCard';
+import Card from '@/app/components/card/project/ProjectCard';
+import { convertFromDatabaseImageURL } from '@/services/supabase/imageHostConverter';
+import type { TeamProjectType } from '@/app/(box-layout)/team/types';
+
+interface ProjectGridProps {
+  projects: CardProps[] | TeamProjectType[];
+  className?: string;
+  isTeamProject?: boolean;
+}
+
+const ProjectGrid = ({
+  projects,
+  className,
+  isTeamProject = false,
+}: ProjectGridProps) => {
+  const transformedProjects: CardProps[] = projects.map((project) => {
+    if (isTeamProject) {
+      // TeamProjectType 변환
+      const teamProject = project as TeamProjectType;
+      return {
+        id: teamProject.project_id,
+        title: teamProject.project_name,
+        description: teamProject.description,
+        projectImage: convertFromDatabaseImageURL(
+          teamProject.project_thumbnail,
+        ),
+        ownerName: teamProject.profile.profile_name,
+        isTeam: teamProject.profile.is_team,
+        authors: teamProject.project_contributors.map((contributor) => ({
+          name: contributor.profile.profile_name,
+          profileImage: convertFromDatabaseImageURL(
+            contributor.profile.profile_image,
+          ),
+        })),
+      };
+    } else {
+      // CardProps 형식 그대로 사용
+      return project as CardProps;
+    }
+  });
+
+  return (
+    <div className={`grid gap-6 grid-cols-auto-fit-card ${className}`}>
+      {transformedProjects.map((data) => (
+        <Card
+          key={data.id}
+          id={data.id}
+          title={data.title}
+          description={data.description}
+          projectImage={data.projectImage}
+          ownerName={data.ownerName}
+          isTeam={data.isTeam}
+          authors={data.authors}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProjectGrid;
