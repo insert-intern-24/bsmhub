58357acfcf2fc98af05e46396cd05307569365c6
gamzamandu <galbimandu1126@gmail.com>
2025-12-01 03:43:23 +0900

    [I25-394] fix: add smooth scrolling to main layout in viewer component

--- src/app/(viewer)/layout.tsx
+++ src/app/(viewer)/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function BoxLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="w-full min-h-dvh pt-4 mt-14 mobile:mt-0 mobile:pt-0">
+    <main className="scroll-smooth w-full min-h-dvh pt-4 mt-14 mobile:mt-0 mobile:pt-0">
       <div className="max-w-[109rem] bg-white mobile:p-3 min-h-screen mobile:pb-12 mx-auto p-[4rem]">
         {children}
       </div>
